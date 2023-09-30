5871ce67443d36494b9625236f3c7abbe8bfcc52
Shehab Taher <shehab.taher93@gmail.com>
2023-09-30 04:23:17 +0300

    create Sidebar

---
+++ components/Sidebar.js
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import React from "react";
+import ImgLogo from "../public/Logo_of_Twitter.svg";
+import SidebarMenuItem from "./SidebarMenuItem";
+
+import {
+  BellIcon,
+  BookmarkIcon,
+  ClipboardIcon,
+  DotsCircleHorizontalIcon,
+  HashtagIcon,
+  HomeIcon,
+  InboxIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+
+const Sidebar = () => {
+  return (
+    <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full">
+      {/* Twitter Logo */}
+      <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
+        <Image src={ImgLogo} width="50" height="50" alt="Logo" />
+      </div>
+      {/* Menu */}
+      <div className="mt-4 mb-2.5 xl:items-start">
+        <SidebarMenuItem text="Home" Icon={HomeIcon} active />
+        <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
+        <SidebarMenuItem text="Notification" Icon={BellIcon} />
+        <SidebarMenuItem text="Message" Icon={InboxIcon} />
+        <SidebarMenuItem text="Bookmark" Icon={BookmarkIcon} />
+        <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
+        <SidebarMenuItem text="Profile" Icon={UserIcon} />
+        <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} />
+      </div>
+      {/* Button */}
+      <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">Tweet</button>
+
+      {/* Mini-Profile */}
+
+      <div className="hoverEffect text-gray-700 flex  items-center justify-center xl:justify-start mt-auto">
+        <img
+          src="https://static.vecteezy.com/system/resources/previews/002/275/847/original/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg"
+          alt="Profile Pic"
+          className="h-10 w-10 rounded-full xl:mr-2"
+        />
+        <div className="leading-5 hidden xl:inline">
+          <h4 className="font-bold">Shehab </h4>
+          <p className="text-gray-500">@ShehabTaher</p>
+        </div>
+        <DotsCircleHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
