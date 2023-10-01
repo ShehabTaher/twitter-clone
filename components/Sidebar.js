2d44e97c1c3e050a6ef754993c58552a777865be
Shehab Taher <shehab.taher93@gmail.com>
2023-10-01 06:42:23 +0300

    Session and modify sidebar and input section

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -13,44 +13,62 @@ import {
   InboxIcon,
   UserIcon,
 } from "@heroicons/react/outline";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Sidebar = () => {
+  const { data: session } = useSession();
   return (
     <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24">
       {/* Twitter Logo */}
       <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
         <Image src={ImgLogo} width="50" height="50" alt="Logo" />
       </div>
+
       {/* Menu */}
+
       <div className="mt-4 mb-2.5 xl:items-start">
         <SidebarMenuItem text="Home" Icon={HomeIcon} active />
         <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
-        <SidebarMenuItem text="Notification" Icon={BellIcon} />
-        <SidebarMenuItem text="Message" Icon={InboxIcon} />
-        <SidebarMenuItem text="Bookmark" Icon={BookmarkIcon} />
-        <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
-        <SidebarMenuItem text="Profile" Icon={UserIcon} />
-        <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} />
+        {session && (
+          <>
+            <SidebarMenuItem text="Notification" Icon={BellIcon} />
+            <SidebarMenuItem text="Message" Icon={InboxIcon} />
+            <SidebarMenuItem text="Bookmark" Icon={BookmarkIcon} />
+            <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
+            <SidebarMenuItem text="Profile" Icon={UserIcon} />
+            <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} />
+          </>
+        )}
       </div>
+
       {/* Button */}
-      <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
-        Tweet
-      </button>
-
-      {/* Mini-Profile */}
-
-      <div className="hoverEffect text-gray-700 flex  items-center justify-center xl:justify-start mt-auto">
-        <img
-          src="https://static.vecteezy.com/system/resources/previews/002/275/847/original/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg"
-          alt="Profile Pic"
-          className="h-10 w-10 rounded-full xl:mr-2"
-        />
-        <div className="leading-5 hidden xl:inline">
-          <h4 className="font-bold">Shehab </h4>
-          <p className="text-gray-500">@ShehabTaher</p>
-        </div>
-        <DotsCircleHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
-      </div>
+      {session ? (
+        <>
+          <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
+            Tweet
+          </button>
+          <div className="hoverEffect text-gray-700 flex  items-center justify-center xl:justify-start mt-auto">
+            <img
+              onClick={signOut}
+              src={session?.user.image}
+              alt="Profile Pic"
+              className="h-10 w-10 rounded-full xl:mr-2"
+            />
+            <div className="leading-5 hidden xl:inline">
+              <h4 className="font-bold">{session.user.name}</h4>
+              <p className="text-gray-500">@{session.user.username}</p>
+            </div>
+            <DotsCircleHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
+          </div>
+        </>
+      ) : (
+        <button
+          onClick={signIn}
+          className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline"
+        >
+          Sign In
+        </button>
+      )}
     </div>
   );
 };
