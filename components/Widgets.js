0914056fd62a7b29f4494a6f6b0e9b1e0442d16d
Shehab Taher <shehab.taher93@gmail.com>
2023-10-01 04:37:32 +0300

    User section in widgets

--- components/Widgets.js
+++ components/Widgets.js
@@ -4,12 +4,14 @@ import { useEffect, useState } from "react";
 import News from "./News";
 
 const Widgets = () => {
-  const [data, setData] = useState(null);
+  const [articles, setArticles] = useState(null);
+  const [users, setUsers] = useState(null);
   const [isLoading, setLoading] = useState(true);
   const [articleNum, setArticleNum] = useState(3);
+  const [userNum, setUserNum] = useState(3);
 
   useEffect(() => {
-    async function fetchData() {
+    async function fetchArticles() {
       try {
         const response = await fetch(
           "https://saurav.tech/NewsAPI/everything/cnn.json"
@@ -18,22 +20,38 @@ const Widgets = () => {
           throw new Error("Network response was not ok");
         }
         const result = await response.json();
-        setData(result);
+        setArticles(result);
         setLoading(false);
       } catch (error) {
         console.error("Error:", error);
         setLoading(false);
       }
     }
-
-    fetchData();
+    async function fetchUsers() {
+      try {
+        const response = await fetch(
+          "https://randomuser.me/api/?results=30&inc=name,login,picture"
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const result = await response.json();
+        setUsers(result);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error:", error);
+        setLoading(false);
+      }
+    }
+    fetchUsers();
+    fetchArticles();
   }, []);
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
-  if (!data) {
+  if (!articles) {
     return <div>Data not available.</div>;
   }
 
@@ -52,11 +70,44 @@ const Widgets = () => {
 
       <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[70%]">
         <h4 className="font-bold text-xl px-4">What's happening</h4>
-        {data?.articles.slice(0, articleNum).map((article) => (
+        {articles?.articles.slice(0, articleNum).map((article) => (
           <News key={article.title} article={article} />
         ))}
         <button
           onClick={() => setArticleNum(articleNum + 3)}
+          className="text-blue-300 pl-4 pb-3 hover:text-blue-400"
+        >
+          Show more
+        </button>
+      </div>
+      <div className="sticky top-16 text-gray-700 space-y-3 bg-gray-100 pt-2 rounded-xl w-[90%] xl:w-[70%]">
+        <h4 className="font-bold text-xl px-4">Who to follow</h4>
+        {users?.results.slice(0, userNum).map((user) => (
+          <div
+            key={user.login.username}
+            className="flex items-center px-4 py-2 cursor-pointer hover:bg-gray-200"
+          >
+            <img
+              className="rounded-full"
+              width="40"
+              src={user.picture.thumbnail}
+              alt=""
+            />
+            <div className="truncate ml-4 leading-5">
+              <h4 className="font-bold hover:underline text-[14px] truncate">
+                {user.login.username}
+              </h4>
+              <h5 className="text-[13px] text-gray-500 truncate">
+                @{user.name.first + "" + user.login.last}
+              </h5>
+            </div>
+            <button className="ml-auto bg-black text-white rounded-full text-sm px-3.5 py-1.5 font-bold">
+              Follow
+            </button>
+          </div>
+        ))}
+        <button
+          onClick={() => setUserNum(userNum + 3)}
           className="text-blue-300 pl-4 pb-3 hover:text-blue-400"
         >
           Show more
